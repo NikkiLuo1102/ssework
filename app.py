a7c201deeda8c61df6d8fc93b7e1c56ccf07463a
NikkiLuo1102 <147421165+NikkiLuo1102@users.noreply.github.com>
2023-10-24 00:16:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+
+@app.route("/query")
+def process_query(q):
+    if q=="dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    else:
+        return "Unknown"
 
