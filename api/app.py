272e82537910a47af2deeac87ab29690c0bd828f
nl1023 <nl1023@imperial.ac.uk>
2023-10-30 23:06:14 +0000

    game first

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import re
+import math
 app = Flask(__name__)
 
 
@@ -20,10 +22,82 @@ def get_query_parameter():
     return process_query(query_parameter)
 
 
+def is_prime(i):
+    if i <= 1 or i % 2 == 0 or i % 3 == 0:
+        return False
+    elif i <= 3:
+        return True
+    else:
+        k = 5
+        while k * k <= i:
+            if i % k == 0 or i % (k + 2) == 0:
+                return False
+            k += 6
+        return True
+
+
 def process_query(query_parameter):
-    if query_parameter == "dinosaurs":
-        return "Dinosaurs ruled the Earth 200 million years ago"
+    pattern_multiplied = r'What is \d+ multiplied by \d+\?$'
+    pattern_largest = r'Which of the following numbers is the largest\
+        : \d+(, \d+)+\?$'
+    pattern_plus = r'What is \d+ plus \d+\?$'
+    pattern_square_cubes = r'Which of the following numbers is both \
+        a square and a cube: \d+(, \d+)+\?$'
+    pattern_prime = r'Which of the following numbers are primes\
+        : \d+(, \d+)+\?$'
+    pattern_minus = r'What is \d+ minus \d+\?$'
+    pattern_num = r'\d+'
+
+    if re.match(pattern_multiplied, query_parameter):
+        matches = re.findall(pattern_num, query_parameter)
+        res = 1
+        for i in matches:
+            res = res * int(i)
+        return str(res)
+
+    elif re.match(pattern_largest, query_parameter):
+        matches = re.findall(pattern_num, query_parameter)
+        int_ls = [int(i) for i in matches]
+        return str(max(int_ls))
+
+    elif re.match(pattern_plus, query_parameter):
+        matches = re.findall(pattern_num, query_parameter)
+        res = 0
+        for i in matches:
+            res = res + int(i)
+        return str(res)
+
+    elif re.match(pattern_square_cubes, query_parameter):
+        matches = re.findall(pattern_num, query_parameter)
+        res = ""
+        for i in matches:
+            square_root = math.sqrt(i)
+            cube_root = i ** (1/3)
+            if square_root.is_integer() and cube_root.is_integer():
+                res = res + str(i) + ", "
+        return res[0:-2]
+
+    elif re.match(pattern_minus, query_parameter):
+        matches = re.findall(pattern_num, query_parameter)
+        res = 0
+        for i in matches:
+            res = res - int(i)
+        return str(res)
+
+    elif re.match(pattern_prime, query_parameter):
+        matches = re.findall(pattern_num, query_parameter)
+        res = ""
+        for i in matches:
+            i = int(i)
+            if is_prime(i):
+                res = res + str(i) + ", "
+        return res[0:-2]
+
     elif query_parameter == "What is your name?":
         return "KFC V50"
+
+    elif query_parameter == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+
     else:
         return "Unknown"
--- api/app_test.py
+++ api/app_test.py
@@ -9,5 +9,33 @@ def test_knows_about_dinosaurs():
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
 
-def tests_knows_about_my_name():
+
+def tests_name():
     assert process_query("What is your name?") == "KFC V50"
+
+
+def tests_multi():
+    assert process_query("What is 2 multiplied by 22?") == "44"
+
+
+def tests_largest():
+    assert process_query("Which of the following numbers is the largest\
+                         : 32, 15, 44?") == "44"
+
+
+def tests_plus():
+    assert process_query("What is 34 plus 12?") == "46"
+
+
+def tests_square_cube():
+    assert process_query("Which of the following numbers is both a \
+                         square and a cube: 78, 64, 16, 27, 729?") == "16, 729"
+
+
+def tests_prime():
+    assert process_query("Which of the following numbers are primes\
+                         : 7 16 23 42 41 128?") == "7, 23, 41"
+
+
+def tests_minus():
+    assert process_query("What is 25 minus 12?") == "13"
